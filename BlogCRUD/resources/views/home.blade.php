f0f3d95225e0563ef38d04c9a8372dea3345f3b4
Norbert <norbert.czubak@wp.pl>
2019-07-07 17:46:00 +0200

    Add MDBoostrap to the project

--- BlogCRUD/resources/views/home.blade.php
+++ BlogCRUD/resources/views/home.blade.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <title>BlogCRUD</title>
 </head>
 <body>
     Blog: <br>
@@ -13,5 +19,13 @@
         <br>
         {{ $post->text }}
     @endforeach
+    <!-- JQuery -->
+    <script type="text/javascript" src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="{{ asset('js/mdb.min.js') }}"></script>
 </body>
 </html>
